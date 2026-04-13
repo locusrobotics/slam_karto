38ff3a605c74b30bd200e7e4358f8621e08b7710
Stephen Williams <swilliams@locusrobotics.com>
2026-04-13 10:45:44 -0700

    Added LoopClosureCallback and SPA Solver back in

--- src/slam_karto.cpp
+++ src/slam_karto.cpp
@@ -61,7 +61,7 @@
 
 #include <slam_karto/SetMapTransform.h>
 #include <slam_karto/loop_closure_callback.h>
-// #include "spa_solver.h"
+#include "spa_solver.h"
 
 #include <boost/algorithm/clamp.hpp>
 #include <boost/circular_buffer.hpp>
@@ -289,7 +289,7 @@ class SlamKarto
     // Karto bookkeeping
     karto::Mapper* mapper_;
     karto::Dataset* dataset_;
-    // SpaSolver* solver_;
+    SpaSolver* solver_;
     // slam_karto::LoopClosureCallback* loop_closure_pauser_;  //!< Listen to loop closure events from karto and pause nav
     std::map<std::string, karto::LaserRangeFinder*> lasers_;
     std::map<std::string, bool> lasers_inverted_;
